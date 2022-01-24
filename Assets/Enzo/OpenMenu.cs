0dc0ce2e1bf0ed23bcb385705cab8657e123d170
Bifflou <gut.enzo02122001@gmail.com>
2022-01-24 14:23:40 +0100

    Ajout table String

--- Assets/Enzo/OpenMenu.cs
+++ Assets/Enzo/OpenMenu.cs
@@ -1,17 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Localization.Settings;
+using UnityEngine.Localization;
 using UnityEngine.UI;
-
 public class OpenMenu : MonoBehaviour
 {
     public GameObject optionMenuUI;
     private bool isOpen = false;
 
+    public void SetSelectedLocale(Locale locale)
+    {
+        LocalizationSettings.SelectedLocale = locale;
+    }
+
     public void Option()
     {
         isOpen = !isOpen;
         optionMenuUI.SetActive(isOpen);
+    }
+
+    public void En()
+    {
+        SetSelectedLocale(LocalizationSettings.AvailableLocales.Locales[0]);
+    }
+
+    public void Fr()
+    {
+        SetSelectedLocale(LocalizationSettings.AvailableLocales.Locales[1]);
     }
 
 }
