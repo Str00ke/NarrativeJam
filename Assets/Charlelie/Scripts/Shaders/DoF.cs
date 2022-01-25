6494057609eaad9923a9c21267735e71d106204e
Str00ke <charlelie.garrigues@edu.devinci.fr>
2022-01-25 12:11:47 +0100

    Update blur

---
+++ Assets/Charlelie/Scripts/Shaders/DoF.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+public class DoF : MonoBehaviour
+{
+    DepthOfField dof;
+    Volume volume;
+    public AnimationCurve anim;
+    public float speed, min, max;
+    bool isBlur = false;
+    bool isUpdate = false;
+    void Start()
+    {
+        volume = GetComponent<Volume>();
+        DepthOfField tmp;
+        if (volume.profile.TryGet<DepthOfField>(out tmp))
+        {
+            dof = tmp;
+        }
+        dof.focalLength.value = min;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) 
+        {
+            if (isUpdate) return;
+            if (isBlur) UnBlur();
+            else Blur();
+            isBlur = !isBlur;
+        }
+    }
+
+
+
+    public void Blur()
+    {
+        Debug.Log("BLUR");
+        StartCoroutine(apply(min, max));
+    }
+
+    public void UnBlur()
+    {
+        Debug.Log("UNBLUR");
+        StartCoroutine(apply(max, min));
+    }
+
+    IEnumerator apply(float s, float e)
+    {
+        isUpdate = true;
+        float t = 0;
+        while (t < 1)
+        {
+            dof.focalLength.value = Mathf.Lerp(s, e, t);
+            t += Time.deltaTime * speed * anim.Evaluate(t);
+            yield return null;
+        }
+        isUpdate = false;
+        yield return null;
+    }
+}
